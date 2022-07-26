243eb2e0c86f130c07fed8173b217097c7b8da1e
nihatcanertug <nihatcanertug@gmail.com>
2022-07-26 23:16:21 +0300

    Business Katmani eklendi.

---
+++ TODEB_DotnetCore_Project/DataAccessLayer/Abstract/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TODEB_DotnetCore_Project.EntityLayer.Entities.Concrete;
+
+namespace TODEB_DotnetCore_Project.DataAccessLayer.Abstract
+{
+    public interface ICustomerRepository
+    {
+        public IEnumerable<Customer> GetAll();
+        public void Insert(Customer customer);
+        public void Update(Customer customer);
+        public void Delete(Customer customer);
+    }
+}
---
+++ TODEB_DotnetCore_Project/DataAccessLayer/Concrete/CustomerRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TODEB_DotnetCore_Project.DataAccessLayer.Abstract;
+using TODEB_DotnetCore_Project.EntityLayer.Entities.Concrete;
+
+namespace TODEB_DotnetCore_Project.DataAccessLayer.Concrete
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private ApplicationDbContext context;
+        public CustomerRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public void Delete(Customer customer)
+        {
+            context.Customers.Remove(customer);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return context.Customers.ToList();
+        }
+
+        public void Insert(Customer customer)
+        {
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
+
+        public void Update(Customer customer)
+        {
+            context.Customers.Update(customer);
+            context.SaveChanges();
+        }
+    }
+}
